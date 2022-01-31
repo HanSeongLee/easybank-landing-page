83acd2fab18da3faf04da24dba4e557f26d3351d
HanSeong Lee <jkhanseong@gmail.com>
2022-01-31 19:53:57 +0900

    ADD | Footer

---
+++ components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './style.module.scss';
+import Container from "../Container";
+import Logo from "../Logo";
+import Button from "../Button";
+import SocialLinkContainer from "../../containers/SocialLinkContainer";
+import SitemapContainer from "../../containers/SitemapContainer";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <Container className={styles.container}>
+                <Logo/>
+
+                <ul className={styles.socialLinkContainer}>
+                    <SocialLinkContainer/>
+                </ul>
+
+                <ul className={styles.sitemapContainer}>
+                    <SitemapContainer />
+                </ul>
+
+                <div>
+                    <Button>
+                        Request Invite
+                    </Button>
+                </div>
+                <p className={styles.copyright}>
+                    © Easybank. All Rights Reserved
+                </p>
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
